0612590ee3b9edc4cb1bde3e8d96078a06c7ccd4
Nexxa <an_na_rib@mail.ru>
2025-10-06 11:49:44 +0700

    Add main file

---
+++ CV-3-12.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from barcode_detection import load_image, preprocess_image, find_barcode_contours, draw_barcodes, detect_barcode
+
+
+def main():
+    return 0;
+
+if __name__ == "__main__":
+    main()
+
+
